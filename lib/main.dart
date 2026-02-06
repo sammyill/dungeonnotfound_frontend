7d987214ed5bdaf7f4d670189b540c6a719f4a22
samuele illuminati <sammyill91@gmail.com>
2026-02-06 16:41:44 +0100

    reworking

--- lib/main.dart
+++ lib/main.dart
@@ -1,51 +1,189 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
+class ImageButtonData extends ChangeNotifier {
+  String _selectedId;
+  final List<Map<String, String>> _buttons;
+
+  ImageButtonData({required List<Map<String, String>> buttons})
+    : _buttons = buttons,
+      _selectedId = buttons.isNotEmpty ? buttons[0]['id']! : '';
+
+  String get selectedId => _selectedId;
+
+  List<Map<String, String>> get buttons => _buttons;
+
+  void selectButton(String id) {
+    if (_selectedId != id) {
+      _selectedId = id;
+      notifyListeners();
+    }
+  }
+}
+
+class ImgButton extends StatelessWidget {
+  final String id;
+  final String imageUrl;
+  final bool isSelected;
+  final VoidCallback onTap;
+  final double itemDiameter;
+  final Color itemColor;
+  final double borderThickness;
+  final double lineThickness;
+
+  const ImgButton({
+    super.key,
+    required this.id,
+    required this.imageUrl,
+    this.isSelected = false,
+    required this.onTap,
+    this.itemDiameter = 90.0,
+    this.itemColor = const Color.fromARGB(255, 194, 145, 1),
+    this.borderThickness = 4.0,
+    this.lineThickness = 4.0,
+  });
+
+  static const double _kVerticalSpacing = 8.0;
+
+  @override
+  Widget build(BuildContext context) {
+    final double innerSize = itemDiameter - (borderThickness * 2);
+
+    return GestureDetector(
+      onTap: onTap,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Container(
+            width: itemDiameter,
+            height: itemDiameter,
+
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              border: isSelected
+                  ? Border.all(color: itemColor, width: borderThickness)
+                  : Border.all(color: itemColor, width: 2),
+            ),
+            child: ClipOval(
+              child: ColoredBox(
+                color: Colors.grey.shade200,
+                child: Image.network(
+                  imageUrl,
+                  width: innerSize,
+                  height: innerSize,
+                  fit: BoxFit.cover,
+                  errorBuilder: (context, error, stackTrace) {
+                    return const Center(
+                      child: Icon(
+                        Icons.image_not_supported,
+                        size: 32,
+                        color: Colors.black45,
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ),
+          ),
+          if (isSelected)
+            Container(
+              margin: const EdgeInsets.only(top: _kVerticalSpacing),
+              width: itemDiameter * 0.9,
+              height: lineThickness,
+              decoration: BoxDecoration(
+                color: itemColor,
+                borderRadius: BorderRadius.circular(lineThickness / 2),
+              ),
+            ),
+        ],
+      ),
+    );
+  }
+}
+
+class HeroPage extends StatelessWidget {
+  final String selectedId;
+
+  const HeroPage({super.key, required this.selectedId});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: const Color.fromARGB(255, 68, 34, 0),
+        border: Border.all(
+          color: const Color.fromARGB(255, 189, 88, 0),
+          width: 2,
+        ),
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          const Text(
+            'Hero Page (dummy)',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 18,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          const SizedBox(height: 8),
+          Text(
+            'Selected button: $selectedId',
+            style: const TextStyle(color: Colors.white70),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+    return ChangeNotifierProvider<ImageButtonData>(
+      create: (BuildContext context) => ImageButtonData(
+        buttons: <Map<String, String>>[
+          <String, String>{
+            'id': 'option1',
+            'imageUrl': 'https://picsum.photos/seed/option1/200',
+          },
+          <String, String>{
+            'id': 'option2',
+            'imageUrl': 'https://picsum.photos/seed/option2/200',
+          },
+          <String, String>{
+            'id': 'option3',
+            'imageUrl': 'https://picsum.photos/seed/option3/200',
+          },
+          <String, String>{
+            'id': 'option4',
+            'imageUrl': 'https://picsum.photos/seed/option4/200',
+          },
+        ],
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        ),
+        home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      ),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -54,68 +192,77 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Consumer<ImageButtonData>(
+              builder:
+                  (
+                    BuildContext context,
+                    ImageButtonData imageData,
+                    Widget? child,
+                  ) {
+                    if (imageData.buttons.isEmpty) {
+                      return const SizedBox.shrink();
+                    }
+
+                    return Container(
+                      decoration: BoxDecoration(
+                        color: const Color.fromARGB(255, 121, 57, 0),
+                        border: Border.all(
+                          color: const Color.fromARGB(255, 189, 88, 0),
+                          width: 2,
+                        ),
+                      ),
+                      padding: const EdgeInsets.all(8),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: imageData.buttons.map<Widget>((
+                            Map<String, String> buttonMap,
+                          ) {
+                            final String id = buttonMap['id']!;
+                            final String imageUrl = buttonMap['imageUrl']!;
+                            final bool isSelected = imageData.selectedId == id;
+
+                            return Padding(
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 8,
+                              ),
+                              child: ImgButton(
+                                id: id,
+                                imageUrl: imageUrl,
+                                isSelected: isSelected,
+                                onTap: () => imageData.selectButton(id),
+                                itemDiameter: 90.0,
+                              ),
+                            );
+                          }).toList(),
+                        ),
+                     
+                    );
+                  },
+            ),
+            const SizedBox(height: 16),
+            Consumer<ImageButtonData>(
+              builder:
+                  (
+                    BuildContext context,
+                    ImageButtonData imageData,
+                    Widget? child,
+                  ) {
+                    return HeroPage(selectedId: imageData.selectedId);
+                  },
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
